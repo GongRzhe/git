76dec3fface55a0e0efb04d68bee816f948984ba
GongRzhe <gongrzhe@gmail.com>
2021-10-11 16:43:57 +0800

    add code
    add code

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,1 +1,22 @@
-hello
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+
+    public GameObject game;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform a = game.transform;
+        Debug.Log(a.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
